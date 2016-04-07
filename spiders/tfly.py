224a0fdd29b008243a1081b75097f36e907fd750
Mark Knudtsen <mark.knudtsen@gmail.com>
2016-04-07 14:10:20 -0700

    updated spider

--- spiders/tfly.py
+++ spiders/tfly.py
@@ -26,7 +26,7 @@ class Recursive(CrawlSpider):
         loader = EventLoader(item, response=response)
         loader.add_xpath('age_restriction', './/p[starts-with(@class, "age-restriction")]/text()')
         loader.add_value('age_restriction', 'NA')
-        loader.add_xpath('promoter',        './/p[@class="event-sponsor"]/text()')
+        loader.add_xpath('promoter',        './/p[starts-with(@class, "event-sponsor")]/text()')
         loader.add_value('promoter',        'NA')
 
         # Complete the loader, yielding the completed item
@@ -40,27 +40,27 @@ class Recursive(CrawlSpider):
         #iterate over each event
         for event in selector.xpath(self.events_list_xpath):
             loader = EventLoader(EventItem(), selector=event)
-            loader.add_xpath('title',       './a/img[@class="event-results-image"]/@title')
-            loader.add_xpath('event_url',   './a/@href')
-            loader.add_xpath('artists',     './/div[@class="event-results-titles"]/h3[@class="headliners summary"]/'
+            loader.add_xpath('title',       './/a/img[@class="event-results-image"]/@title')
+            loader.add_xpath('event_url',   './/div[starts-with(@class, "event-results-content")]/a/@href')
+            loader.add_xpath('artists',     './/div[starts-with(@class, "event-results-titles")]/h3[starts-with(@class, "headliners summary")]/'
                                             'a/text()')
-            loader.add_xpath('artists',     './/div[@class="event-results-titles"]/'
-                                            'p[@class="event-results-openers description"]/a/text()')
-            loader.add_xpath('event_ts',    './/div[@class="event-date dtstart"]/span[@class="value-title"]/@title')
-            loader.add_xpath('ticket_price','.//div[@class="event-results-ticket-price"]/'
-                                            'p[@class="event-price"]/text()')
+            loader.add_xpath('artists',     './/div[starts-with(@class, "event-results-titles")]/'
+                                            'p[starts-with(@class, "event-results-openers description")]/a/text()')
+            loader.add_xpath('event_ts',    './/div[starts-with(@class, "event-date dtstart")]/span[starts-with(@class, "value-title")]/@title')
+            loader.add_xpath('ticket_price','.//div[starts-with(@class, "event-results-ticket-price")]/'
+                                            'p[starts-with(@class, "event-price")]/text()')
             loader.add_value('ticket_price','NA')
-            loader.add_xpath('status',      './/div[@class="event-results-ticket-price"]/'
-                                            'span[@class="button radius small sold-out"]/text()')
-            loader.add_xpath('status',      './/div[@class="event-results-ticket-price"]/'
-                                            'span[@class="ticket-link primary-link"]/'
-                                            'a[@class="button radius small green tickets"]/text()')
-            loader.add_xpath('status',      './/div[@class="event-results-ticket-price"]/'
+            loader.add_xpath('status',      './/div[starts-with(@class, "event-results-ticket-price")]/'
+                                            'span[starts-with(@class, "button radius small sold-out")]/text()')
+            loader.add_xpath('status',      './/div[starts-with(@class, "event-results-ticket-price")]/'
+                                            'span[starts-with(@class, "ticket-link primary-link")]/'
+                                            'a[starts-with(@class, "button radius small green tickets")]/text()')
+            loader.add_xpath('status',      './/div[starts-with(@class, "event-results-ticket-price")]/'
                                             'span[starts-with(@class, "button radius")]/text()')        
             loader.add_value('status',      'NA')            
-            loader.add_xpath('purchase_url','.//div[@class="event-results-ticket-price"]/'
-                                            'span[@class="ticket-link primary-link"]/'
-                                            'a[@class="button radius small green tickets"]/@href')
+            loader.add_xpath('purchase_url','.//div[starts-with(@class, "event-results-ticket-price")]/'
+                                            'span[starts-with(@class, "ticket-link primary-link")]/'
+                                            'a[starts-with(@class, "button radius small green tickets")]/@href')
             loader.add_value('purchase_url','NA')
             loader.add_value('venue_id',     str(venue_id))
             #loader.add_value('last_update',  str(date.today()))
